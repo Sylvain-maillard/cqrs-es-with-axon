f66a671c46e7102b3999fff1a424907028a5aa0f
Sylvain Maillard <smaillard@oui.sncf>
2018-11-08 10:29:51 +0100

    Initialisation axon 4.0.1

--- src/main/java/com/prez/axon/Bank.java
+++ src/main/java/com/prez/axon/Bank.java
@@ -1,12 +1,20 @@
 package com.prez.axon;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class Bank {
+public class Bank implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Bank.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.debug("Hello, Axon world");
     }
 }
